d8dac81b8ee869556cbfde428f53c861319ea800
toby <tobyljj@gmail.com>
2016-06-20 16:42:27 -0400

    separated chathead class

--- app/src/main/java/edu/cmu/hcii/sugilite/SugiliteAccessibilityService.java
+++ app/src/main/java/edu/cmu/hcii/sugilite/SugiliteAccessibilityService.java
@@ -35,13 +35,14 @@ import java.util.Set;
 import edu.cmu.hcii.sugilite.model.AccessibilityNodeInfoList;
 import edu.cmu.hcii.sugilite.automation.*;
 import edu.cmu.hcii.sugilite.model.block.SugiliteStartingBlock;
+import edu.cmu.hcii.sugilite.ui.StatusIcon;
 
 public class SugiliteAccessibilityService extends AccessibilityService {
-    private ImageView statusIcon;
-    private WindowManager windowManager;
+        private WindowManager windowManager;
     private SharedPreferences sharedPreferences;
     private Automator automator;
     private SugiliteData sugiliteData;
+    private StatusIcon statusIcon;
 
     public SugiliteAccessibilityService() {
     }
@@ -52,10 +53,11 @@ public class SugiliteAccessibilityService extends AccessibilityService {
         windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         sugiliteData = (SugiliteData)getApplication();
+        statusIcon = new StatusIcon(this, sugiliteData, sharedPreferences);
         automator = new Automator(sugiliteData);
         try {
             Toast.makeText(this, "Sugilite Accessibility Service Started", Toast.LENGTH_SHORT).show();
-            addStatusIcon();
+            statusIcon.addStatusIcon();
         }
         catch (Exception e){
             e.printStackTrace();
@@ -126,7 +128,7 @@ public class SugiliteAccessibilityService extends AccessibilityService {
         Toast.makeText(this, "Sugilite Accessibility Service Stopped", Toast.LENGTH_SHORT).show();
         if(statusIcon != null)
             try {
-                windowManager.removeView(statusIcon);
+                statusIcon.removeStatusIcon();
             }
             catch (Exception e){
                 //failed to remove status icon
@@ -135,159 +137,10 @@ public class SugiliteAccessibilityService extends AccessibilityService {
         //windowManager.removeView(statusIcon);
     }
 
-    private void addStatusIcon(){
-        statusIcon = new ImageView(this);
-        final Spinner spinner = new Spinner(this);
-        statusIcon.setImageResource(R.mipmap.ic_launcher);
-        statusIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
 
 
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_PHONE,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT);
-
-        DisplayMetrics displaymetrics = new DisplayMetrics();
-        windowManager.getDefaultDisplay().getMetrics(displaymetrics);
 
 
-        params.gravity = Gravity.TOP | Gravity.RIGHT;
-        params.x = 0;
-        params.y = 200;
-        addCrumpledPaperOnTouchListener(statusIcon, params, displaymetrics, windowManager);
-
-        //NEEDED TO BE CONFIGURED AT APPS->SETTINGS-DRAW OVER OTHER APPS on API>=23
-        int currentApiVersion = android.os.Build.VERSION.SDK_INT;
-        if(currentApiVersion >= 23){
-            checkDrawOverlayPermission();
-            if(Settings.canDrawOverlays(getApplicationContext()))
-                windowManager.addView(statusIcon, params);
-        }
-        else {
-            windowManager.addView(statusIcon, params);
-        }
-    }
-
-    /**
-     * make the chathead draggable. ref. http://blog.dision.co/2016/02/01/implement-floating-widget-like-facebook-chatheads/
-     * @param view
-     * @param mPaperParams
-     * @param displayMetrics
-     * @param windowManager
-     */
-    private void addCrumpledPaperOnTouchListener(final View view, final WindowManager.LayoutParams mPaperParams, DisplayMetrics displayMetrics, final WindowManager windowManager) {
-        final int windowWidth = displayMetrics.widthPixels;
-        view.setOnTouchListener(new View.OnTouchListener() {
-            private int initialX;
-            private int initialY;
-            private float initialTouchX;
-            private float initialTouchY;
-            GestureDetector gestureDetector = new GestureDetector(getApplicationContext(), new SingleTapUp());
-
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (gestureDetector.onTouchEvent(event)) {
-                    // single tap
-                    AlertDialog.Builder textDialogBuilder = new AlertDialog.Builder(getApplicationContext());
-                    boolean recordingInProcess = sharedPreferences.getBoolean("recording_in_process", false);
-                    textDialogBuilder.setTitle("STATUS: " + (recordingInProcess ? "RECORDING:" : "NOT RECORDING") + "\nChoose Operation:");
-                    if (recordingInProcess) {
-                        SugiliteStartingBlock startingBlock = (SugiliteStartingBlock) sugiliteData.getScriptHead();
-                        String[] operations = {"View Current Script: " + (startingBlock == null ? "NULL" : startingBlock.getScriptName()), "End Recording", "Quit Sugilite"};
-                        textDialogBuilder.setItems(operations, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                switch (which) {
-                                    case 0:
-                                        Toast.makeText(getApplicationContext(), "view current script", Toast.LENGTH_SHORT).show();
-                                        break;
-                                    case 1:
-                                        Toast.makeText(getApplicationContext(), "end recording", Toast.LENGTH_SHORT).show();
-                                        break;
-                                    case 2:
-                                        Toast.makeText(getApplicationContext(), "quit sugilite", Toast.LENGTH_SHORT).show();
-                                        break;
-                                }
-                            }
-                        });
-                    } else {
-                        String[] operations = {"View Script List", "Quit Sugilite"};
-                        textDialogBuilder.setItems(operations, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                switch (which) {
-                                    case 0:
-                                        Toast.makeText(getApplicationContext(), "view  script list", Toast.LENGTH_SHORT).show();
-                                        break;
-                                    case 1:
-                                        Toast.makeText(getApplicationContext(), "quit sugilite", Toast.LENGTH_SHORT).show();
-                                        break;
-                                }
-                            }
-                        });
-                    }
-                    Dialog dialog = textDialogBuilder.create();
-                    dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
-                    dialog.show();
-                    return true;
-
-                }
-
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        initialX = mPaperParams.x;
-                        initialY = mPaperParams.y;
-                        initialTouchX = event.getRawX();
-                        initialTouchY = event.getRawY();
-                        return true;
-                    case MotionEvent.ACTION_UP:
-                        return true;
-                    case MotionEvent.ACTION_MOVE:
-                        // move paper ImageView
-                        mPaperParams.x = initialX + (int) (initialTouchX - event.getRawX());
-                        mPaperParams.y = initialY + (int) (event.getRawY() - initialTouchY);
-                        windowManager.updateViewLayout(view, mPaperParams);
-                        return true;
-                }
-                return false;
-            }
-
-            class SingleTapUp extends GestureDetector.SimpleOnGestureListener {
-
-                @Override
-                public boolean onSingleTapUp(MotionEvent event) {
-                    return true;
-                }
-            }
-
-        });
-    }
-
-    /** code to post/handler request for permission */
-    public final static int REQUEST_CODE = -1010101;
-
-    public void checkDrawOverlayPermission() {
-        /** check if we already  have permission to draw over other apps */
-        int currentApiVersion = android.os.Build.VERSION.SDK_INT;
-        if(currentApiVersion >= 23) {
-            if (!Settings.canDrawOverlays(getApplicationContext())) {
-                /** if not construct intent to request permission */
-                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                        Uri.parse("package:" + getPackageName()));
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                /** request permission via start activity for result */
-                startActivity(intent);
-
-            }
-        }
-    }
 
 
     private Intent generatePopUpActivityIntentFromEvent(AccessibilityEvent event){

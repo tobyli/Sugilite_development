0d4c1a3409144ab4843af387466ac63682372885
Brandon Canfield <sneelhorses@gmail.com>
2019-07-16 13:55:51 -0400

    Create subhashes for strings of 2-4 tokens

--- app/src/main/java/edu/cmu/hcii/sugilite/sharing/HashedSplitStringGenerator.java
+++ app/src/main/java/edu/cmu/hcii/sugilite/sharing/HashedSplitStringGenerator.java
@@ -7,7 +7,23 @@ public class HashedSplitStringGenerator {
     }
 
     public HashedSplitString generate(String s) {
-        // TODO tokenize/heuristically remove PII
-        return new HashedSplitString(new HashedString(s), new ArrayList<>(0));
+        ArrayList<HashedSubString> substrings = new ArrayList<>();
+        String[] pieces = s.split("\\s");
+        if (pieces.length >= 2 && pieces.length <= 4) {
+            for (int skip = 0; skip < pieces.length; skip++) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < pieces.length; i++) {
+                    if (skip == i) {
+                        sb.append("<pii>");
+                        sb.append(' ');
+                    } else {
+                        sb.append(pieces[i]);
+                        sb.append(' ');
+                    }
+                }
+                substrings.add(new HashedSubString(sb.toString(), 1));
+            }
+        }
+        return new HashedSplitString(new HashedString(s), substrings);
     }
 }

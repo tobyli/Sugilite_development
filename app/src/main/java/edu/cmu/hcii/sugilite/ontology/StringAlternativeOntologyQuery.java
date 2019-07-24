b34aa810f37416ad5c13813ec1d722a2dbb8a3dd
Brandon Canfield <sneelhorses@gmail.com>
2019-07-24 14:02:29 -0400

    Add StringAlternativeOntologyQuery

---
+++ app/src/main/java/edu/cmu/hcii/sugilite/ontology/StringAlternativeOntologyQuery.java
@@ -0,0 +1,47 @@
+package edu.cmu.hcii.sugilite.ontology;
+
+import edu.cmu.hcii.sugilite.sharing.StringAlternativeGenerator;
+import edu.cmu.hcii.sugilite.source_parsing.SugiliteScriptExpression;
+
+import java.util.Set;
+
+public class StringAlternativeOntologyQuery extends OntologyQuery {
+    protected SugiliteRelation r = null;
+    private StringAlternativeGenerator.StringAlternative alt;
+
+    public StringAlternativeOntologyQuery(SugiliteRelation r, StringAlternativeGenerator.StringAlternative alt) {
+        this.r = r;
+        this.alt = alt;
+    }
+
+
+    @Override
+    protected boolean overallQueryFunction(SugiliteEntity currNode, UISnapshot graph) {
+        Set<SugiliteTriple> sugiliteTriples = graph.getSubjectTriplesMap().get(currNode.getEntityId());
+        if (sugiliteTriples != null) {
+            for (SugiliteTriple triple : sugiliteTriples) {
+                if (triple.getPredicate().equals(r)) {
+                    Set<StringAlternativeGenerator.StringAlternative> alts = StringAlternativeGenerator.generateAlternatives(triple.getObjectStringValue());
+                    for (StringAlternativeGenerator.StringAlternative a : alts) {
+                        // TODO maybe also check if priority is equal?
+                        if (a.altText.equals(this.alt.altText)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        // TODO add to parser
+        return "(patternMatch " + r.getRelationName() + " " + SugiliteScriptExpression.addQuoteToTokenIfNeeded(alt.altText) + ")";
+    }
+
+    @Override
+    public OntologyQuery clone() {
+        return new StringAlternativeOntologyQuery(r, alt.clone());
+    }
+}

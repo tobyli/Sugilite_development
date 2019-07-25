a67bfaab0925eef0e347750fb1f9d836ee3ba99f
Brandon Canfield <sneelhorses@gmail.com>
2019-07-25 14:51:09 -0400

    Make StringInContext its own class

---
+++ app/src/main/java/edu/cmu/hcii/sugilite/sharing/StringInContext.java
@@ -0,0 +1,40 @@
+package edu.cmu.hcii.sugilite.sharing;
+
+import com.google.api.client.repackaged.com.google.common.base.Objects;
+
+public class StringInContext {
+    public String activityName;
+    public String packageName;
+    public String text;
+
+    public StringInContext(String activityName, String packageName, String text) {
+        this.activityName = activityName;
+        this.packageName = packageName;
+        this.text = text;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(text, activityName, packageName);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof StringInContext) {
+            StringInContext sic = (StringInContext)obj;
+            return text.equals(sic.text) && activityName.equals(sic.activityName) && packageName.equals(sic.packageName);
+        }
+        return false;
+    }
+
+    public String toJson() {
+        StringBuilder sb = new StringBuilder("{");
+        sb.append("\"package\": \"" + packageName + "\"");
+        sb.append(",\"activity\": \"" + activityName + "\"");
+        sb.append(",\"text_hash\": \"" + new HashedString(text).toString() + "\"");
+        //sb.append(",\"priority\": " + this.priority);
+        //sb.append(",\"index\": " + index);
+        sb.append("}");
+        return sb.toString();
+    }
+}
